65a742f4e098ab049557cb824940403ce6de67b7
Alex Carpenter <alexanderpcarpenter@gmail.com>
2022-01-25 22:38:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//add your code here to submit the form
+// 1. Listen for the form submission event
+// 2. Grab the contents of the fields
+// 3. Send a POST request to the Forms endpoint to submit the form data to HubSpot
