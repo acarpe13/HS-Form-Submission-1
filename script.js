3cb1952bcee860bf0c8afa11681783b319a3c4a4
Alex Carpenter <alexanderpcarpenter@gmail.com>
2022-01-28 21:17:24 -0500

    refactors-further
    Removes more relics of previous version

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 var params_obj1 = {
   title: "Objective 1",
   subtitle: "Submit form directly to HS API",
-  // url: "https://api.hsforms.com/submissions/v3/integration/submit/21334118/876e460d-12e3-4430-b61a-98e9bc54c56f"
-  url: "https://webhook.site/d31b760b-650c-4dc9-a42a-e506013da95f"
+  url: "https://api.hsforms.com/submissions/v3/integration/submit/21334118/876e460d-12e3-4430-b61a-98e9bc54c56f"
+  // url: "https://webhook.site/d31b760b-650c-4dc9-a42a-e506013da95f"
 };
 
 var params_obj2 = {
@@ -21,119 +21,6 @@ var form_params = {
   subtitle: "",
   url: ""
 };
-
-// Wait until document is loaded
-$(document).ready(function(){
-  // On form submit action execute this code
-  $("#form-submission").submit(function(event){
-    // Ignore default form submit behavior
-    event.preventDefault()
-    // Construct body of API call
-    var data = {
-      "fields": [
-        {
-          "objectTypeId": "0-1",
-          "name": "email",
-          "value": $("#email").val()
-        },
-        {
-          "objectTypeId": "0-1",
-          "name": "firstname",
-          "value": $("#firstname").val()
-        },
-        {
-          "objectTypeId": "0-1",
-          "name": "lastname",
-          "value": $("#lastname").val()
-        }
-      ]
-    };
-
-    var url = form_params.url;
-
-    // Settings for ajax call
-    var settings = {
-      "url": url,
-      "method": "POST",
-      "timeout": 0,
-      "headers": {
-        "Content-Type": "application/json"
-      },
-      "data": JSON.stringify(data),
-
-      // Display Feedback based on response code
-      statusCode: {
-        200: function(response) {
-          $(".message-header").text("Success");
-          $(".message-body").html(`<p>${response.inlineMessage}<p>`);
-          $("#submit-message").attr("class","message is-success");
-          $("#submit-response").removeAttr("hidden");
-          // console.log(response.inlineMessage);
-        },
-        400: function(response) {
-          var res = response.responseJSON
-          $(".message-header").text(res.message);
-
-          $(".message-body").html(`<p>Hubspot Correlation Id: ${res.correlationId}</p>`)
-          $(".message-body").append(`<p>Error Type: ${res.errors[0].errorType}</p>`)
-          $(".message-body").append(`<p>Error Message: ${res.errors[0].message}</p>`)
-
-          $("#submit-message").attr("class","message is-danger");
-          $("#submit-response").removeAttr("hidden");
-
-        },
-        403: function(xhr) {
-          $(".message-header").text("Status: 403");
-          $(".message-body").html(`${response.responseText}`);
-
-          $("#submit-message").attr("class","message is-danger");
-          $("#submit-response").removeAttr("hidden");
-        },
-        404: function(xhr) {
-          $(".message-header").text("Status: 404");
-          $(".message-body").html(`${response.responseText}`);
-
-          $("#submit-message").attr("class","message is-danger");
-          $("#submit-response").removeAttr("hidden");
-        },
-      }
-    };
-
-    // Execute POST request
-    $.ajax(settings)
-
-  })
-
-})
-
-// Tab Navigation
-function openTab(evt, tabName) {
-  var i, x, tablinks;
-  // get tab content elements
-  x = document.getElementsByClassName("content-tab");
-  // for each element set display to none (hide)
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  // get tab elements
-  tablinks = document.getElementsByClassName("tab");
-  // set all tabs to inactive
-  for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-  }
-  // set current content to display on (unhide)
-  console.log(tabName);
-  if(tabName=="Objective1"){
-    form_params = params_obj1;
-  } else if (tabName=="Objective2"){
-    form_params = params_obj2;
-  } else {
-    form_params = {};
-  }
-  document.getElementById(tabName).style.display = "block";
-  // set current tab to active
-  evt.currentTarget.className += " is-active";
-}
 
 class ResponseMessage extends HTMLElement {
   connectedCallback(){
